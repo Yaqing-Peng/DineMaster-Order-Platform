6918548b8e0560857be1ee4125466afd1c1d0b6a
Yaqing-Peng <759560274@qq.com>
2023-10-05 23:24:22 -0700

    add files

--- src/main/java/com/example/springbbotweb/controller/HelloController.java
+++
@@ -1,2 +0,0 @@
-package com.example.springbbotweb.controller;public class HelloController {
-}
--- src/test/java/com/example/springbootweb/SpringbootWebApplicationTests.java
+++ src/test/java/com/example/springbootweb/SpringbootWebApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.springbbotweb;
+package com.example.springbootweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbbotWebApplicationTests {
+class SpringbootWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
