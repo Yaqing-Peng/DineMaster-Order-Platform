09cd9ddb72e0d24bde2d7a5b5934e2b96383ef87
Yaqing-Peng <759560274@qq.com>
2023-10-10 17:38:22 -0700

    modify sql

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,7 +1,8 @@
-create table tb_menu
+create database if not exists db01;
+
+create table db01.tb_menu
 (
-    dish_id     int auto_increment comment 'dish id'
-    primary key,
+    id     int auto_increment comment 'dish id'  primary key,
     dish_name   varchar(50)      not null,
     description varchar(500)     not null,
     price       double unsigned  not null comment 'unit price',
@@ -10,33 +11,30 @@ create table tb_menu
     status      int              not null comment '1 available 2 unavailable',
     create_time datetime         not null,
     update_time datetime         not null,
-    constraint dish_name
-    unique (dish_name)
+    constraint dish_name         unique (dish_name)
     )
     comment 'menu list';
 
 
-create table tb_user
+create table db01.tb_user
 (
-    user_id     int auto_increment
-        primary key,
+    id     int auto_increment  primary key,
     user_name   varchar(20) not null,
     gender      tinyint     not null comment '1 male 2 female',
     birthday    date        not null,
     phone       char(20)    not null,
+    status      int         not null comment '1 active 2 cancelled',
     create_time datetime    not null,
     update_time datetime    not null,
-    constraint user_name
-        unique (user_name)
+    constraint user_name    unique (user_name)
 )
     comment 'user list';
 
 
 
-create table tb_order
+create table db01.tb_order
 (
-    order_id    int      not null
-        primary key,
+    id          int      not null  primary key,
     user_id     int      not null,
     dish_id     int      not null,
     status      int      not null comment'1 ordered 2 not ordered',
